e3a379e3b6c6a105fea86bc59d2c775f17243abc
anastahir-future <282459790+anastahir-future@users.noreply.github.com>
2026-08-05 04:52:16 -0700

    Update app_assets.dart

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -6,4 +6,7 @@ class AppAssets {
   static const String onboarding2 = 'assets/images/onboarding_2.png';
   static const String onboarding3 = 'assets/images/onboarding_3.png';
   static const String languageBannerBird = 'assets/images/language_banner_bird.png';
+  static const String accountCreated = 'assets/images/account_created.png';
+  static const String authBird='assets/images/auth_bird.png';
+
 }
