5839ecff278e039a54be43155c6b271e41551b39
anastahir-future <282459790+anastahir-future@users.noreply.github.com>
2026-08-05 05:33:31 -0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fluentta_ai/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:fluentta_ai/app_navigator.dart';
 import 'package:fluentta_ai/core/storage/local_storage.dart';
@@ -5,6 +7,9 @@ import 'package:fluentta_ai/core/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final localStorage = await LocalStorage.getInstance();
 
   runApp(FluentaApp(localStorage: localStorage));
