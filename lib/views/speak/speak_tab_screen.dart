9b934b16d35d27d23c56edfcb03f5e5324d0049d
anastahir-future <282459790+anastahir-future@users.noreply.github.com>
2026-08-12 22:58:43 -0700

    Update speak_tab_screen.dart

--- lib/views/speak/speak_tab_screen.dart
+++ lib/views/speak/speak_tab_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fluentta_ai/widgets/common/appbar_widget.dart';
 import 'package:fluentta_ai/widgets/home/todays_lesson_card.dart';
 import 'package:flutter/material.dart';
 import 'package:fluentta_ai/core/constants/app_fonts.dart';
@@ -7,7 +8,6 @@ import 'package:fluentta_ai/core/theme/app_colors.dart';
 import 'package:fluentta_ai/views/ai_tutor/ai_tutor_screen.dart';
 import 'package:fluentta_ai/viewmodels/home_view_model.dart';
 import 'package:fluentta_ai/views/pronunciation/pronunciation_flow.dart';
-import 'package:fluentta_ai/widgets/speak/speak_ad_banner.dart';
 import 'package:fluentta_ai/widgets/speak/speak_ai_tutor_card.dart';
 import 'package:fluentta_ai/widgets/speak/speak_pronunciation_tile.dart';
 import 'package:provider/provider.dart';
@@ -23,92 +23,39 @@ class SpeakTabScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackgroundColor,
+      appBar: AppBarWidget(
+        title: 'Speak With AI Tutor',
+      ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(
-                AppSizes.horizontalPadding,
-                AppSizes.spaceMd,
-                AppSizes.horizontalPadding,
-                0,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(
+            AppSizes.horizontalPadding,
+            AppSizes.spaceMd,
+            AppSizes.horizontalPadding,
+            AppSizes.spaceLg,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: AppSizes.sp(8),
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      l10n.speakWithAiTutorTitle,
-                      style: TextStyle(
-                        fontFamily: AppFonts.plusJakartaSans,
-                        fontSize: AppSizes.sp(22),
-                        fontWeight: FontWeight.w700,
-                        color: AppColors.primaryColor,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: AppSizes.w(12),
-                      vertical: AppSizes.h(6),
-                    ),
-                    decoration: BoxDecoration(
-                      color: AppColors.homeCardLavender,
-                      borderRadius: BorderRadius.circular(AppSizes.w(20)),
-                    ),
-                    child: Row(
-                      children: [
-                        Text(
-                          '$lives',
-                          style: TextStyle(
-                            fontFamily: AppFonts.plusJakartaSans,
-                            fontSize: AppSizes.sp(14),
-                            fontWeight: FontWeight.w700,
-                            color: AppColors.textPrimary,
-                          ),
-                        ),
-                        SizedBox(width: AppSizes.w(4)),
-                        Icon(
-                          Icons.favorite,
-                          color: AppColors.heartRed,
-                          size: AppSizes.sp(16),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+              SpeakAiTutorCard(
+                onStartChat: () => AiTutorScreen.open(context),
               ),
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                padding: EdgeInsets.fromLTRB(
-                  AppSizes.horizontalPadding,
-                  AppSizes.spaceMd,
-                  AppSizes.horizontalPadding,
-                  AppSizes.spaceLg,
-                ),
-                child: Column(
-                  children: [
-                    SpeakAiTutorCard(
-                      onStartChat: () => AiTutorScreen.open(context),
+              SizedBox(height: AppSizes.h(14)),
+              SpeakPronunciationTile(
+                onTap: () {
+                  Navigator.of(context).push<void>(
+                    MaterialPageRoute<void>(
+                      builder: (_) => const PronunciationFlow(),
                     ),
-                    SizedBox(height: AppSizes.h(14)),
-                    SpeakPronunciationTile(
-                      onTap: () {
-                        Navigator.of(context).push<void>(
-                          MaterialPageRoute<void>(
-                            builder: (_) => const PronunciationFlow(),
-                          ),
-                        );
-                      },
-                    ),
-                    SizedBox(height: AppSizes.h(130)),
-                    const HomeBannerAd(),
-                  ],
-                ),
+                  );
+                },
               ),
-            ),
-          ],
+              SizedBox(height: AppSizes.h(130)),
+              const HomeBannerAd(),
+            ],
+          ),
         ),
       ),
     );
